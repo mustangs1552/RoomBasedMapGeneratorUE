8640717c552e1384452c5823dac77abbdbe0f046
Matthew George <mustangs155222@gmail.com>
2024-07-30 22:19:35 -0400

    #71 Updated to UE 5.4 and removed Auto Versioning plugin.

--- Source/RBMGeneratorEditor.Target.cs
+++ Source/RBMGeneratorEditor.Target.cs
@@ -8,7 +8,7 @@ public class RBMGeneratorEditorTarget : TargetRules
 	public RBMGeneratorEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		
 		ExtraModuleNames.AddRange( new string[] { "RBMGenerator" } );
