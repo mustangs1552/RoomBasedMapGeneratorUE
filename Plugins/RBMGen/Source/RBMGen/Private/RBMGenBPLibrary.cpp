5b0cc4b8c6298fa8c4580ec33f3462df84246443
Matthew George <mustangs155222@gmail.com>
2024-12-22 14:07:41 -0500

    #74 Commiting lingering changes.

---
+++ Plugins/RBMGen/Source/RBMGen/Private/RBMGenBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RBMGenBPLibrary.h"
+#include "RBMGen.h"
+
+URBMGenBPLibrary::URBMGenBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float URBMGenBPLibrary::RBMGenSampleFunction(float Param)
+{
+	return -1;
+}
+
